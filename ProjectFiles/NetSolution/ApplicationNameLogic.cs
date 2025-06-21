e8e51e95f60350aa3ff6b19d2623a422c25e1a4a
wawelk <wawelk@reynco.com>
2025-06-20 20:47:23 -0500

    Initial commit

---
+++ ProjectFiles/NetSolution/ApplicationNameLogic.cs
@@ -0,0 +1,26 @@
+#region Using directives
+using FTOptix.HMIProject;
+using FTOptix.NetLogic;
+using FTOptix.UI;
+using FTOptix.SerialPort;
+using FTOptix.MicroController;
+using FTOptix.CommunicationDriver;
+using FTOptix.RAEtherNetIP;
+using FTOptix.DataLogger;
+using FTOptix.Store;
+using FTOptix.InfluxDBStoreLocal;
+using FTOptix.InfluxDBStore;
+#endregion
+
+public class ApplicationNameLogic : BaseNetLogic
+{
+    public override void Start()
+    {
+        Label label = Owner as Label;
+        label.Text = Project.Current.BrowseName;
+    }
+
+    public override void Stop()
+    {
+    }
+}
